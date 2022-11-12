2c9526461ef06d651ff3b9cef2e067212a0be6f3
Shreyans Gandhi <shreyans.p.gandhi@gmail.com>
2022-11-11 22:33:13 -0800

    fix: fix index file

--- scripts/build-index-files.js
+++ scripts/build-index-files.js
@@ -4,7 +4,7 @@ const fileNames = fs.readdirSync('./dist/tokens')
   .filter(fileName => fileName.includes('.js'))
   .map(fileName => fileName.replace('.js', ''));
 
-const exportStatements = fileNames.map(fileName => `export{default as ${fileName}} from './tokens/${fileName};'`).join('\n');
+const exportStatements = fileNames.map(fileName => `export{default as ${fileName}} from './tokens/${fileName}';`).join('\n');
 
 fs.writeFileSync('dist/index.js', exportStatements);
 fs.writeFileSync('dist/index.d.ts', exportStatements);
