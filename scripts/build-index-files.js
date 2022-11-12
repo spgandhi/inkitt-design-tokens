e13443ca149c4bb5f8925b94476dfc773ad2671a
Shreyans Gandhi <shreyans.p.gandhi@gmail.com>
2022-11-11 22:00:20 -0800

    fix: update default exports

--- scripts/build-index-files.js
+++ scripts/build-index-files.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 
-const indexFileContents = '';
-
-const filesPath = fs.readdirSync('./dist/tokens')
+const fileNames = fs.readdirSync('./dist/tokens')
   .filter(fileName => fileName.includes('.js'))
   .map(fileName => fileName.replace('.js', ''));
 
-const importStatements = filesPath.map(fileName => `import ${fileName} from './tokens/${fileName};'`).join('\n');
-const exportStatements = `\n\nexport default { ${filesPath.join(',')} }`;
+const exportStatements = fileNames.map(fileName => `export{default as ${fileName}} from './tokens/${fileName};'`).join('\n');
 
-fs.writeFileSync('dist/index.js', importStatements + exportStatements);
-fs.writeFileSync('dist/index.d.ts', importStatements + exportStatements);
-
+fs.writeFileSync('dist/index.js', exportStatements);
+fs.writeFileSync('dist/index.d.ts', exportStatements);
